a9511d5d761880e3df32d70b81e0fdf1160443e8
André Padez <andre.padez@gmail.com>
2015-12-21 17:42:21 +0000

    working

---
+++ src/index.js
@@ -0,0 +1,47 @@
+var sEXITs = [
+  'exit',
+  'SIGHUP',
+  'SIGINT',
+  'SIGQUIT',
+  'SIGILL',
+  'SIGTRAP',
+  'SIGABRT',
+  'SIGIOT',
+  'SIGBUS',
+  'SIGFPE',
+  'SIGKILL',
+  'SIGUSR1',
+  'SIGSEGV',
+  'SIGUSR2',
+  'SIGPIPE',
+  'SIGALRM',
+  'SIGTERM',
+  'SIGCHLD',
+  'SIGSTKFLT',
+  'SIGCONT',
+  'SIGSTOP',
+  'SIGTSTP',
+  'SIGBREAK',
+  'SIGTTIN',
+  'SIGTTOU',
+  'SIGURG',
+  'SIGXCPU',
+  'SIGXFSZ',
+  'SIGVTALRM',
+  'SIGPROF',
+  'SIGWINCH',
+  'SIGIO',
+  'SIGPOLL',
+  'SIGLOST',
+  'SIGPWR',
+  'SIGSYS',
+  'SIGUNUSED'
+]
+
+module.exports = {
+  on: (cb) => {
+    for (var i = 0, l = sEXITs.length; i < l; i++) {
+      process.on(sEXITs[i], cb)
+    }
+  }
+}
